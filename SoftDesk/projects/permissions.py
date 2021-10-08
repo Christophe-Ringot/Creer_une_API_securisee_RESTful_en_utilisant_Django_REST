cdb8dd4326662f370fc08bc74498b557238a5a4c
Christophe Ringot <christophe.ringot1996@gmail.com>
2021-10-08 15:01:40 +0200

    ajout des permissions

---
+++ SoftDesk/projects/permissions.py
@@ -0,0 +1,111 @@
+from rest_framework import permissions
+from .models import Contributor
+
+
+class ProjectPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        try:
+            info_c = Contributor.objects.get(
+                user_id=request.user, project_id=obj
+                )
+        except Contributor.DoesNotExist:
+            return False
+
+        if info_c.permission == "Al":
+            return True
+        elif info_c.permission == "Rd":
+            return request.method in permissions.SAFE_METHODS
+
+
+class ContributorPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        id_p = view.kwargs.get("project_pk")
+        try:
+            info_c = Contributor.objects.get(
+                user_id=request.user, project_id=id_p
+                )
+        except Contributor.DoesNotExist:
+            return False
+        if info_c.permission in ["Al", "Rd"]:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        id_p = view.kwargs.get("project_pk")
+        try:
+            info_c = Contributor.objects.get(
+                user_id=request.user, project_id=id_p
+                )
+        except Contributor.DoesNotExist:
+            return False
+        if info_c.permission == "Al":
+            return request.method in ["DELETE"]
+        elif info_c.permission == "Rd":
+            if request.user == obj.user_id:
+                return True
+            else:
+                return request.method in permissions.SAFE_METHODS
+
+
+class IssuePermission(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        id_p = view.kwargs.get("project_pk")
+        try:
+            info_c = Contributor.objects.get(
+                user_id=request.user, project_id=id_p
+                )
+        except Contributor.DoesNotExist:
+            return False
+        if info_c.permission in ["Al", "Rd"]:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        id_p = view.kwargs.get("project_pk")
+        try:
+            info_c = Contributor.objects.get(
+                user_id=request.user, project_id=id_p
+                )
+        except Contributor.DoesNotExist:
+            return False
+        if info_c.permission == "Al":
+            return True
+        elif info_c.permission == "Rd":
+            if request.user == obj.author_user_id:
+                return True
+            else:
+                return request.method in permissions.SAFE_METHODS
+
+
+class CommentPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        id_p = view.kwargs.get("project_pk")
+        try:
+            info_c = Contributor.objects.get(
+                user_id=request.user, project_id=id_p
+                )
+        except Contributor.DoesNotExist:
+            return False
+        if info_c.permission in ["Al", "Rd"]:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        id_p = view.kwargs.get("project_pk")
+        try:
+            info_c = Contributor.objects.get(
+                user_id=request.user, project_id=id_p
+                )
+        except Contributor.DoesNotExist:
+            return False
+        if info_c.permission == "Al":
+            return True
+        elif info_c.permission == "Rd":
+            if request.user == obj.author_user_id:
+                return True
+            else:
+                return request.method in permissions.SAFE_METHODS
